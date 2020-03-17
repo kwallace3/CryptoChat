1dd2fa494e3aab0ce3ea56e2f1b1336d4e9ae90a
zschaub <bdzschau@gmail.com>
2020-03-17 18:16:45 -0400

    #68 Added message splitting

--- Server/Server.py
+++ Server/Server.py
@@ -2,7 +2,7 @@ import _thread
 import socket
 
 myHost = 'localhost'
-myPort = 6667
+myPort = 4995
 
 # Creates a TCP Server with Port# 6667
 sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,10 +10,44 @@ sockobj.bind((myHost, myPort))
 sockobj.listen(16)
 
 
+def splitmessage(message):
+    split = [""]
+    # Remove the message before the first '%' and save it as split[0]. This is the message header
+    for i in range(len(message)):
+        if message[i] == '%':
+            split[0] = message[:i]
+            message = message[i + 1:]
+            break
+
+    # Checks the header was successfully taken off, if not return empty split
+    if split == [""]:
+        return split
+
+    x = 1
+    while x > 0:
+        print(message)
+        if split[0] == 'senddirectmessage' and x == 3:
+            size = 3
+        else:
+            size = 2
+        length = int(message[:size])
+        split.append(message[size:length + size])
+
+        x += 1
+        # breaks out of loop if next character is not '%' or we are at the end of the message
+        if message[length + size:] == '':
+            break
+        if message[length + size] != '%':
+            break
+        message = message[length + size + 1:]
+
+    return split
+
+
 class Account:
     account_list = []
 
-    def __init(self, login, password):
+    def __init__(self, login, password):
         pass
 
     # Check is username is in use by an existing account
@@ -23,7 +57,7 @@ class Account:
 
     # First checks if username is in use then makes account
     @staticmethod
-    def create_account(connection, username, password):
+    def create_account(connection, username, email, password):
         pass
 
     @staticmethod
@@ -43,13 +77,25 @@ def send_message(sender, receiver, message):
 
 # Finds action message wants to takes and evokes method
 def handle_message(connection, message):
-    pass
+    split = splitmessage(message)
+    print(split)
+    if len(split) == 1:
+        connection.sendall("error%24Incorrect Message Format".encode())
+        return
+    if split[0] == "createaccount" and len(split) >= 4:
+        Account.create_account(connection, split[1], split[2], split[3])
+    elif split[0] == "login" and len(split) >= 3:
+        Account.create_login(connection, split[1], split[2])
+    elif split[0] == "logout" and len(split) >= 2:
+        Account.create_logout(connection, split[1])
+    else:
+        connection.sendall("error%24Incorrect Message Format".encode())
 
 
 def handle_client(connection):
     data = connection.recv(1024).decode()
     print("Received from new connection:" + data)
-
+    handle_message(connection, data)
     connection.sendall(data.encode())
     connection.close()
 
