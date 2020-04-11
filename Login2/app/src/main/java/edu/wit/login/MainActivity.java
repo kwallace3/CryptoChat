c19e7aa107e121d67fa05c756516490b8573d9e8
Kevin <channingk@wit.edu>
2020-04-11 11:57:32 -0400

    Skeleton channel

---
+++ Login2/app/src/main/java/edu/wit/login/MainActivity.java
@@ -0,0 +1,63 @@
+package edu.wit.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Executor;
+
+//reference: https://www.tutorialspoint.com/android/android_login_screen.htm
+public class MainActivity extends AppCompatActivity {
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView register = findViewById(R.id.CAbutton);
+        register.setMovementMethod(LinkMovementMethod.getInstance());
+        register.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Register.class);
+            startActivity(intent);
+        });
+
+        TextView login = findViewById(R.id.Lbutton);
+        login.setMovementMethod(LinkMovementMethod.getInstance());
+        login.setOnClickListener(v -> {
+            Intent intent1 = new Intent(MainActivity.this, MainPage.class);
+            startActivity(intent1);
+            //execute();
+        });
+}
+
+
+  /*  public void execute() {
+        Executor executor = (Executor) new MainActivity();
+        executor.execute( () -> {
+            try {
+                final String user = findViewById(R.id.Username).toString();
+                final String pass = findViewById(R.id.Password).toString();
+                NetworkOPS connect = new NetworkOPS();
+                if(connect.retrieve(user,pass).equals("login%7success%##username")){
+                    Intent intent1 = new Intent(MainActivity.this, MainPage.class);
+                    startActivity(intent1);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+*/
+}
---
+++ Login2/app/src/main/java/edu/wit/login/NetworkOPS.java
@@ -0,0 +1,81 @@
+package edu.wit.login;
+
+import android.annotation.SuppressLint;
+import androidx.appcompat.app.AppCompatActivity;
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+// reference:https://www.tutorialspoint.com/sending-and-receiving-data-with-sockets-in-android
+@SuppressLint("SetTextI18n")
+public class NetworkOPS extends AppCompatActivity {
+
+    public Socket execute() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(() -> {
+            try {
+                InetAddress address = InetAddress.getByName("cryptochatwit.duckdns.org");
+                Socket socket = new Socket(address, 4995);
+                return socket;
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        });
+        return null;
+    }
+    //gets user data
+    public String[] retrieve() throws IOException {
+       Socket connect = execute();
+        String[] data = new String[10];
+        InputStream is = connect.getInputStream();
+        InputStreamReader message = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(message);
+        for(int i = 0; i<data.length;i++){
+            data[i] = br.readLine();
+        }
+        return data;
+    }
+    // gets the username and password from the server
+    public String retrieve(String user, String pass) throws Exception{
+        Socket connect = execute();
+        InputStream is = connect.getInputStream();
+        InputStreamReader message = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(message);
+        System.out.println(br);
+        String response = br.readLine();
+
+        if (response.contains(user) && response.contains(pass) ){
+            connect.close();
+            return "login%7success%##username";
+        }
+        else
+            connect.close();
+        return br.toString();
+
+    }
+    //writes to server/database
+    public void write(String username, String Password, String Email) throws Exception{
+        Socket connect = execute();
+        // Get a reference to the socket's output stream.
+        OutputStream os = connect.getOutputStream();
+        //Send data to server
+        String User = findViewById(R.id.newname).toString();
+        String pass = findViewById(R.id.txtPassword).toString();
+        String email = findViewById(R.id.txtEmail).toString();
+
+        os.write(User.getBytes());
+        os.write(pass.getBytes());
+        os.write(email.getBytes());
+
+        connect.close();
+    }
+
+
+}
